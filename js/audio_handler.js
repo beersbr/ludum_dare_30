0b923ae1f7f97ca4f5081e6e25069625b71bc1af
Mramaizng <beersbr@gmail.com>
2014-08-24 14:12:57 -0700

    added usic

--- js/audio_handler.js
+++ js/audio_handler.js
@@ -1,5 +1,21 @@
 var AUDIO = (function(){
 	var audio = {};
 	
+	audio.voices = 16;
+
+	audio.index = 0;
+	audio.tags = [];
+	for(var i = 0; i < audio.voices; i++){
+		var a = new Audio();
+		a.preload = true;
+		audio.tags.push(a);
+	}
+		
+
+	audio.play = function(audioSrc){
+
+	}
+
+
 	return audio;
 }());
