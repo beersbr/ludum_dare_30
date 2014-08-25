5519551e56f1d173e484235f6b9e35507ae70550
Keith Stewart <keith@giantsource.com>
2014-08-25 15:42:15 -0400

    +walrus

--- js/enemies.js
+++ js/enemies.js
@@ -354,6 +354,96 @@ function Snake(args){
 Snake.prototype = new GameObject;
 Snake.constructor = Snake;
 
+/******************************
+ *	Walrus 
+ ******************************/
+function Walrus(args){
+	if(!args) args = {};
+
+	GameObject.call(this, args);
+
+	this.moveSpeed = 10; // pixels per second
+	this.shootSpeed = 0; // per second
+	this.bulletSpeed = 0; // pixels per second
+	this.bulletDamage = 0;
+
+	this.health = 5;
+	this.armor = 0;
+	this.trinkets = [];
+	this.items = [];
+
+	this.drag = 0.85;
+
+	this.shootTime = 0;
+	this.canShoot = false;
+
+	this.collidable = true;
+
+	this.animations = [];
+
+	// add to current level enemies
+	Game.level.enemies.push(this);
+
+	this._update = function(elapsedTime){
+		var speed = this.moveSpeed * elapsedTime;
+
+		if(!this.dying)
+			this.vel = this.vel.add(Game.player.pos.sub(this.pos).normalize().scale(speed));
+
+		this.vel = this.vel.scale(this.drag);
+		this.pos = this.pos.add(this.vel);
+
+		if(this.health <= 0 && !this.dying)
+			this.die();
+
+		if(this.pos.x < 0 || this.pos.x > 800 || this.pos.y < 0 || this.pos.y > 600){
+			console.error("WALRUS POS: ", this.pos);
+			this.dead = true;
+		}
+
+	};
+
+	this._render = function(){
+		this.context.drawImage(this.image, 0, 0, 40, 40, this.pos.x, this.pos.y, this.size.w, this.size.h);
+	}
+
+	this.onCollide = function(o){
+		if(o instanceof Tile){
+			if(o.tileState != "solid")
+				return;
+
+			var v = uncollide(this.getRect(), o.getRect());
+			this.vel = this.vel.add(v);
+		}
+
+		if(o instanceof Bullet){
+			//AUDIO.playHit("hit-walrus");
+			this.addAnimation(new TurnRed(this, 0.3), false);
+
+			var d = this.pos.sub(Game.player.pos).normalize().scale(8.0);
+			this.vel = this.vel.add(d);
+			this.health -= 1;
+		}
+	}
+
+	this.die = function(){
+
+		this.addAnimation(new Shrink(this, 0.5), true);
+		// this._update = function(){};
+		this.collidable = false;
+		this.dying = true;
+
+
+		// TODO: move into game
+		var selfId = this.id;
+		var idx = Game.level.enemies.find(function(id){ return (selfId == id.id);})
+		Game.level.enemies.splice(idx, 1);
+	}
+
+}
+
+Walrus.prototype = new GameObject;
+Walrus.constructor = Walrus;
 
 
 /******************************
--- js/main.js
+++ js/main.js
@@ -155,8 +155,8 @@ function Player(args){
 
 			AUDIO.playHit("hit-ouch");
 
-			this.health -= 1;
-			StatusBar.removeHealth();
+			this.health -= 3;
+			StatusBar.removeHealth({amount:3});
 			this.addAnimation(new TurnRed(this, 0.8), false);
 
 			this.wasHit = true;
@@ -171,8 +171,8 @@ function Player(args){
 
 			AUDIO.playHit("hit-ouch");
 
-			this.health -= 1;
-			StatusBar.removeHealth();
+			this.health -= 2;
+			StatusBar.removeHealth({amount:2});
 			this.addAnimation(new TurnRed(this, 0.8), false);
 
 			this.wasHit = true;
@@ -219,6 +219,20 @@ function Player(args){
 
 			this.health -= 1;
 			StatusBar.removeHealth();
+			this.addAnimation(new TurnRed(this, 0.8), false);
+
+			this.wasHit = true;
+			this.hitTime = this.safeTime;
+		}
+
+		if(o instanceof Walrus){
+			v = uncollide(this.getRect(), o.getRect());
+			this.vel = this.vel.add(v);	
+
+			if(this.wasHit) return;
+
+			this.health -= 2;
+			StatusBar.removeHealth({amount:2});
 			this.addAnimation(new TurnRed(this, 0.8), false);
 
 			this.wasHit = true;
@@ -643,6 +657,7 @@ var Game = (function(){
 	"map-2-1.json",
 	"map-2-2.json",
 	"map-2-3.json",*/
+	"map-3-1.json",
 	"map-4-1.json"
 	]
 
